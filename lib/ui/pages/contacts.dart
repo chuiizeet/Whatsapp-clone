f49e19415c4a6f65c85fab717266afd00ebe97a0
chuiizeet <chuy.guerra19@outlook.es>
2018-11-21 00:26:13 -0600

    List view

--- lib/ui/pages/contacts.dart
+++ lib/ui/pages/contacts.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/chat_model.dart';
+import 'package:whatsapp_clone/ui/pages/chat_screen.dart';
 
 class Contacts extends StatefulWidget {
   _ContactsState createState() => _ContactsState();
@@ -9,9 +11,78 @@ class _ContactsState extends State<Contacts> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Contacts", style: new TextStyle(color: Colors.white)),
+        iconTheme: IconThemeData(
+          color: Colors.white
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.search),
+            color: Colors.white,
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: new Icon(Icons.more_vert),
+            color: Colors.white,
+            onPressed: (){},
+          ),
+        ],
+        title: new Container(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text("Contacts", style: new TextStyle(color: Colors.white)),
+                new Container(
+                  // padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: new Text("5 contacts",
+                  style: new TextStyle(
+                    fontSize: 12.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.white
+                  ),
+                  )
+                )
+              ],
+            ),
+        ),
       ),
-       
+      body: new ListView.builder(
+      itemCount: messageData.length,
+      itemBuilder: (context,i) => new Column(
+        children: <Widget>[
+          new Divider(height: 22.0,),
+          new ListTile(
+            leading: new CircleAvatar(
+              maxRadius: 25,
+              backgroundImage: NetworkImage(messageData[i].imageUrl),
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+              new Text(
+              messageData[i].name,
+                style: new TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              ),
+              new Text(
+                'MOBILE',
+                  style: new TextStyle(
+                    color: Colors.green,
+                    fontSize: 16.0
+                  ),
+                ),
+              ],
+            ),
+            onTap: (){
+              var route = new MaterialPageRoute(
+                builder: (BuildContext context) => ChatScreen(name: messageData[i].name, profileImage: messageData[i].imageUrl)
+              );
+              Navigator.of(context).push(route);
+            },
+          ),
+        ],
+      ),
+    ),   
     );
   }
 }
